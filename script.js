4eb5eb523df59a8b40f272ff96147fc3d6ff83cc
VitalyM87 <makarov.1987@yandex.ru>
2020-09-25 07:22:50 +0300

    js text wrapping

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let h1 = document.querySelector('h1');
-
-h1.onclick = function () {
-  h1.classList.toggle('h1cool')
+function showHide(element_id) {
+  //Если элемент с id-шником element_id существует
+  if (document.getElementById(element_id)) {
+    //Записываем ссылку на элемент в переменную obj
+    var obj = document.getElementById(element_id);
+    //Если css-свойство display не block, то:
+    if (obj.style.display != "block") {
+      obj.style.display = "block"; //Показываем элемент
+    } else obj.style.display = "none"; //Скрываем элемент
+  }
+  //Если элемент с id-шником element_id не найден, то выводим сообщение
+  else alert("Элемент с id: " + element_id + " не найден!");
 }
