f7fe3e75cb07743f42f6a7804c99ba8b2e8d5cbf
VitalyM87 <makarov.1987@yandex.ru>
2020-09-26 04:58:30 +0300

    sunflower edit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+"use strict";
+
 function showHide(element_id) {
   //Если элемент с id-шником element_id существует
   if (document.getElementById(element_id)) {
-    //Записываем ссылку на элемент в переменную obj
-    var obj = document.getElementById(element_id);
+    //Записываем ссылку на элемент в переменную functionTrigger
+    let functionTrigger = document.getElementById(element_id);
     //Если css-свойство display не block, то:
-    if (obj.style.display != "block") {
-      obj.style.display = "block"; //Показываем элемент
-    } else obj.style.display = "none"; //Скрываем элемент
+    if (functionTrigger.style.display != "block") {
+      functionTrigger.style.display = "block"; //Показываем элемент
+    } else functionTrigger.style.display = "none"; //Скрываем элемент
   }
   //Если элемент с id-шником element_id не найден, то выводим сообщение
   else alert("Элемент с id: " + element_id + " не найден!");
