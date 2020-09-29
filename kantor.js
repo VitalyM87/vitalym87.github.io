f22970bac869e08ba6cdce7348a015f080176736
VitalyM87 <makarov.1987@yandex.ru>
2020-09-29 09:04:56 +0300

    loading last stable commit

--- kantor.js
+++ kantor.js
@@ -12,3 +12,21 @@ function counter() {
   alert(counter++);
   alert(counter++);
 }
+
+function summ() {
+  let a = prompt("Первое число?", 1);
+  let b = prompt("Второе число?", 2);
+
+  alert( Number(a) + Number(b));
+}
+
+function question() {
+  let userAnswer = prompt("Какое «официальное» название JavaScript?", "....Script");
+
+  if (userAnswer == "ECMAScript") {
+    alert("Верно")
+  } else {
+    alert("Не знаете? ECMAScript!")
+  }
+
+}
