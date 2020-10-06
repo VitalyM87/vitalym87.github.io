d47b8af92cf15d914cf0b4b4d0f908e53d213bd6
VitalyM87 <makarov.1987@yandex.ru>
2020-10-06 21:04:52 +0300

    geekbrains js course homework 1

---
+++ geekbrains.js
@@ -0,0 +1,15 @@
+function convertTempreture() {
+  let celsius = prompt ("Введите значение температуры в градусах Цельсия" , "0");
+  alert((9 / 5) * celsius + 32);
+}
+
+function nameAdmin() {
+  let name = ("Василий");
+  let admin = name;
+  alert(admin);
+}
+
+function typeTest() {
+  let anyType = (1000 + "108");
+  alert(typeof(anyType));
+}
