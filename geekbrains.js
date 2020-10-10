69b0fba82c6b192e452b884aae5bba4448e7f1eb
VitalyM87 <makarov.1987@yandex.ru>
2020-10-10 16:42:53 +0300

    homework 2

--- geekbrains.js
+++ geekbrains.js
@@ -1,5 +1,5 @@
 function convertTempreture() {
-  let celsius = prompt ("Введите значение температуры в градусах Цельсия" , "0");
+  let celsius = prompt("Введите значение температуры в градусах Цельсия", "0");
   alert((9 / 5) * celsius + 32);
 }
 
@@ -13,4 +13,21 @@ function typeTest() {
   let anyType = (1000 + "108");
   alert(anyType);
   alert(typeof(anyType));
+}
+
+function conditionFirst() {
+  let a = +prompt('Введите значение переменной А', '1');
+  let b = +prompt('Введите значение переменной B', '1');
+  if (a > 0 && b > 0) {
+    let c = (a - b);
+    alert('A и B положительные, их разность равна ' + c);
+  } else {
+    if (a < 0 && b < 0) {
+      c = (a * b);
+      alert('A и B отрицательные, их произведение равно ' + c);
+    } else {
+      c = (a + b);
+      alert('A и B c разными знаками, их сумма равна ' + c);
+    }
+  }
 }
