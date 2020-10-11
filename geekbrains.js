401e384b8cd63722b352ab9fc3c1fb9555eb1a6c
VitalyM87 <makarov.1987@yandex.ru>
2020-10-11 20:19:20 +0300

    hw2done

--- geekbrains.js
+++ geekbrains.js
@@ -42,52 +42,52 @@ function hw2ex4() {
     switch (a) {
       case 0:
         alert('0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 1:
         alert('1 2 3 4 5 6 7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 2:
         alert('2 3 4 5 6 7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 3:
         alert('3 4 5 6 7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 4:
         alert('4 5 6 7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 5:
         alert('5 6 7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 6:
         alert('6 7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 7:
         alert('7 8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 8:
         alert('8 9 10 11 12 13 14 15');
-        brake;
+        break;
       case 9:
         alert('9 10 11 12 13 14 15');
-        brake;
+        break;
       case 10:
         alert('10 11 12 13 14 15');
-        brake;
+        break;
       case 11:
         alert('11 12 13 14 15');
-        brake;
+        break;
       case 12:
         alert('12 13 14 15');
-        brake;
+        break;
       case 13:
         alert('13 14 15');
-        brake;
+        break;
       case 14:
         alert('14 15');
-        brake;
+        break;
       case 15:
         alert('15');
-        brake;
+        break;
     }
   }
 }
@@ -104,28 +104,28 @@ function hw2ex5and6() {
       };
       result = sum(a, b);
       alert(`Сумма чисел ${result}`);
-      brake;
+      break;
     case '-':
       function dif(a, b) {
         return (a - b);
       };
       result = dif(a, b);
       alert(`Разность чисел ${result}`);
-      brake;
+      break;
     case '*':
       function mult(a, b) {
         return (a * b);
       };
       result = mult(a, b);
       alert(`Произведение чисел ${result}`);
-      brake;
+      break;
     case '/':
       function div(a, b) {
         return (a / b);
       };
       result = div(a, b);
       alert(`Частное чисел ${result}`);
-      brake;
+      break;
       default:
       alert('Что-то пошло не так');
   }
@@ -145,4 +145,17 @@ function hw2ex7() {
     default:
       alert('default');
   }
+}
+
+function hw2ex8() {
+
+  function power(val, pow) {
+    if (pow == 1) {
+      return val;
+    } else {
+      return val * power(val, pow - 1);
+    }
+  }
+
+  alert(power(6, 3));
 }
